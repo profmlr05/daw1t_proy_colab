19c31db6db6b80331c731d65d7b40aa0db1a9b78
profmlr05 <mlazarodes@gmail.com>
2025-04-23 11:45:42 +0200

    Añadida documentación java al proyecto

--- Calculadora/src/com/endes/Calculadora.java
+++ Calculadora/src/com/endes/Calculadora.java
@@ -1,31 +1,62 @@
 package com.endes;
+/**
+ * 
+ * Realiza las operaciones matemáticas básicas:
+ * suma, resta, multiplicación y division
+ * @version 1.0
+ * @author Nombre del autor	
+ */
 public class Calculadora {
-    private int num1;
+	private int num1;
     private int num2;
    
-    // ejemplo de subida
+    /**
+     * Iniciar clase
+     * @param a {@code int}
+     * @param b {@code int}
+     */
    public Calculadora(int a, int b) {
        num1 = a;
        num2 = b;
    }
-   //ejemplo
+  /**
+   * Realiza la suma de dos números enteros
+   * @return {@code int}  
+   */
    public int suma() {
        int resul = num1 + num2;
        return resul;
    }
+   /**
+    * Realiza la resta de dos números enteros
+    * @return {@code int} 
+    */
+   
    public int resta() {
        int resul = num1 - num2;
        return resul;
    }
+   /**
+    * Realiza la multiplicación de dos números enteros
+    * @return {@code int} 
+    */
    public int multiplica() {
        int resul = num1 * num2;
        return resul;
    }
+ /**
+  * Realiza la división de dos números enteros
+  * @return {@code int} 
+  */
    public int divide() {
        int resul = num1 / num2;
        return resul;
    }
-   
+   /**
+    * Realiza la división de dos números enteros.
+    * @return {@code int} 
+    * @exception ArithmeticException por 0
+    */
    public int divide0() {		   
 	   if(num2 == 0)
 	      throw new java.lang.ArithmeticException("División por 0");
